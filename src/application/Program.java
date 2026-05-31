3f00a594f156a8f303be12ff92947134bbb34573
ViniciusPaiva20 <viniciuscontact20@gmail.com>
2026-05-31 14:31:13 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDateTime.now(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marthe Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
